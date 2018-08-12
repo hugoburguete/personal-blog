877f0d3ed513d44ad356e8a01f715fa6796f0764
Hugo Burguete <hugoburguete@hotmail.com>
2018-08-12 13:02:08 +0100

    Fixed logout btn

--- resources/views/site/footer.blade.php
+++ resources/views/site/footer.blade.php
@@ -10,7 +10,13 @@
 
 		@if (!Auth::guest())
 			<a href="{{ route('admin.index') }}">Dashboard</a>
-			<a href="{{ route('logout') }}">Logout</a>
+			<a href="{{ route('logout') }}" 
+				onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+				Logout
+			</a>
+            <form id="logout-form" action="{{ url('/logout') }}" method="POST" style="display: none;">
+                {{ csrf_field() }}
+            </form>
 		@endif
 	</section>
 </div>
