c6c562d3088c908f127e2b3ebb313706bce52e9d
Hugo Burguete <hugoburguete@hotmail.com>
2018-12-27 18:00:47 +0000

    Clearing clutter

--- config/services.php
+++ config/services.php
@@ -14,27 +14,6 @@ return [
     |
     */
 
-    'mailgun' => [
-        'domain' => env('MAILGUN_DOMAIN'),
-        'secret' => env('MAILGUN_SECRET'),
-    ],
-
-    'ses' => [
-        'key' => env('SES_KEY'),
-        'secret' => env('SES_SECRET'),
-        'region' => env('SES_REGION', 'us-east-1'),
-    ],
-
-    'sparkpost' => [
-        'secret' => env('SPARKPOST_SECRET'),
-    ],
-
-    'stripe' => [
-        'model' => ProgrammingBlog\User::class,
-        'key' => env('STRIPE_KEY'),
-        'secret' => env('STRIPE_SECRET'),
-    ],
-
     'google' => [
         'webmastertools' => env('GOOGLE_WEBMASTER_TOOLS_KEY'),
     ],
