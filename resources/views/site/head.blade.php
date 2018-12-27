2495bbb790680d55e7edfaecd7df912edf3178a2
Hugo Burguete <hugoburguete@hotmail.com>
2018-12-27 12:20:29 +0000

    Removed unnecessary font

--- resources/views/site/head.blade.php
+++ resources/views/site/head.blade.php
@@ -6,9 +6,6 @@
 {!! SEO::generate(config('app.env') === 'live') !!}
 @yield('site_meta')
 
-{{-- Fonts --}}
-<link href="https://fonts.googleapis.com/css?family=Play|Open+Sans:300,300i,400,400i,600,600i" rel="stylesheet">
-
 {{-- App stylesheets --}}
 <link rel="stylesheet" href="{{ url('/css/app.css') }}">
 @yield('head_styles')
